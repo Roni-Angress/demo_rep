49978a3ca51783bcabe70a9c987d9267ebc245c3
Roni-Angress <roniangressmaple@gmail.com>
2023-08-08 16:39:05 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! from Roni! v2! every time an update to the app file is pushed, it will be automaticly updated at the host url!'
+    return 'Hello, World! from Roni! v2! every time an update to the app file is pushed, it will be automaticly updated at the host url! to check this out, just edit this file, push the changes and see for yourself'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
