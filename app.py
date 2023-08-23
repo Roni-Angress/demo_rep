25beae66bd036d6bcf1485bfa8f77c0af0f174a1
Roni-Angress <roniangressmaple@gmail.com>
2023-08-23 17:22:21 +0300

    modified:   app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Check out this cool website! Made by Roni.A.'
+    return 'Check out this cool website! Made by Roni.A. adding another text'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
